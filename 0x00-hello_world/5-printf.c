4d2a3ae47d187bf5df097cf4ef5221013df67c20
Nourhan Ahmed <nourhan222000@gmail.com>
2023-03-16 17:54:57 -0700

    My sixth commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point.
+ *
+ * Description: A C program that prints a sentence using the function printf.
+ *
+ * Return: 0 (Success).
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
