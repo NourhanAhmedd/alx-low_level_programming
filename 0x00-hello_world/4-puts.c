c7db14c3052161c0771701e3c0d1a3c9dcf19654
Nourhan Ahmed <nourhan222000@gmail.com>
2023-03-16 17:47:31 -0700

    My fifth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point.
+ *
+ * Description: A C program that prints a sentence using the function puts.
+ *
+ * Return: 0 (Success).
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
