8eb982e4ae76c2fd82f10ff2636cbc21cf48c89a
Nourhan Ahmed <nourhan222000@gmail.com>
2023-03-17 17:57:32 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point.
+ *
+ * Description: Write a program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: 0 (Success).
+*/
+
+int main(void)
+{
+	char character = 'a';
+
+	while (character <= 'z')
+	{
+		putchar(character);
+		character++;
+	}
+	putchar('\n');
+	return (0);
+}
