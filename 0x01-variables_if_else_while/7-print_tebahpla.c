4447333cc2a783f01e71d2d46ab928932103273f
Nourhan Ahmed <nourhan222000@gmail.com>
2023-03-17 18:39:54 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point.
+ *
+ * Description: Write a program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ *
+ * Return: 0 (Success).
+*/
+
+int main(void)
+{
+	char character = 'z';
+
+	while (character >= 'a')
+	{
+		putchar(character);
+		character++;
+	}
+	putchar('\n');
+	return (0);
+}
