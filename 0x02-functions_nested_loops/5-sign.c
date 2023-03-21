219d93a77bd93601906e169828cc09d791a82612
Nourhan Ahmed <nourhan222000@gmail.com>
2023-03-21 16:40:58 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main"
+
+/**
+ * print_sign - Function that prints the sign of a number.
+ *
+ * @number: Takes input for the function.
+ *
+ * Return: 1 (If the first condition is true)
+ *         , 0 (If the second condition is true)
+ *         or -1 (If the third condition is true).
+*/
+
+int print_sign(int number)
+{
+	if (number > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (number == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
