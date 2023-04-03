ffaa4c6e51739422eccff212c35f9d87a5b6c8fb
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-03 15:32:35 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,18 +12,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	char *pointer1;
+	char *pointer2;
+
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *pointer1 = haystack;
-		char *pointer2 = needle;
+		*pointer1 = haystack;
+		*pointer2 = needle;
 
 		while (*pointer1 == *pointer2 && *pointer2 != '\0')
 		{
-			*pointer1++;
-			*pointer2++;
+			pointer1++;
+			pointer2++;
 		}
 		if (*pointer2 == '\0')
+		{
 			return (haystack);
+		}
 	}
 	return (0);
 }
