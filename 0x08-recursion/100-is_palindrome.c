817e59121ab89072b03d7171e82e7e9c5c4bf1e5
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-05 18:30:52 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Function that returns 1 if a string is
+ * a palindrome and 0 if not.
+ *
+ * @s: Takes input for the function.
+ *
+ * Return: It will return (check_palindrome(s, 0, e - 1, e % 2)).
+*/
+
+int is_palindrome(char *s)
+{
+	int e = index(s);
+
+	return (check_palindrome(s, 0, e - 1, e % 2));
+}
+
+/**
+ * check_palindrome - Function that checks the palindrome.
+ *
+ * @s: Takes input for the function.
+ * @i: An integer (Start - from right to left).
+ * @e: An integer (End - from left to right).
+ * @m: An integer (mod).
+ *
+ * Return: It will return (check_palindrome(s, i + 1, e - 1, mod)).
+*/
+
+int check_palindrome(char *s, int i, int e, int m)
+{
+	if ((i == e && m != 0) || (i == e + 1 && m == 0))
+	{
+		return (1);
+	}
+	else if (s[i] != s[e])
+	{
+		return (0);
+	}
+	else
+	{
+		return (check_palindrome(s, i + 1, e - 1, mod));
+	}
+}
+
+/**
+ * index - Function that prints the last index.
+ *
+ * @s: Takes input for the function.
+ *
+ * Return: It will return (number).
+*/
+
+int index(char *s)
+{
+	int number;
+
+	number = 0;
+	if (*s > '\0')
+	{
+		number = number + index(s + 1) + 1;
+	}
+	return (number);
+}
