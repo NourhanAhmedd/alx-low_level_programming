9b73386a11889f0cbd9fcc031a06e0b063e58bd7
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-08 13:59:39 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_string - Check the string.
+ *
+ * @s: The string (array).
+ *
+ * Return: 0 (Success).
+*/
+
+int check_string(char *s)
+{
+	unsigned int length;
+
+	length = 0;
+	while (length; < strlen(s))
+	{
+		if (!isdigit(s[length]))
+		{
+			return (0);
+		}
+		length++;
+	}
+	return (1);
+}
+
+/**
+ * main - Write a program that adds positive numbers.
+ *
+ * @argc: Argument Count.
+ * @argv: Argument Vector.
+ *
+ * Return: 0 (Success).
+*/
+
+int main(int argc, char *argv[])
+{
+	int length;
+	int number;
+	int SUM;
+
+	length = 1;
+	SUM = 0;
+	while (length < argc)
+	{
+		if (check_string(argv[length]))
+		{
+			number = atoi(argv[length]);
+			SUM = SUM + number;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		length++;
+	}
+	printf("%d\n", SUM);
+	return (0);
+}
