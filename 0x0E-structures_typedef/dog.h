6d7273c2b8a21b378da6f411ee8323f60c9a8d9c
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-17 15:56:23 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_FILE_H
+#define DOG_FILE_H
+/**
+ * struct dog - Define a new type struct dog with the following elements.
+ *
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
