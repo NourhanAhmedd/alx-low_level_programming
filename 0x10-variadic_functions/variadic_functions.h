2bd8f7f9c857835f23150d08c17ad592dafbbeba
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-20 20:13:57 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,20 +2,20 @@
 #define V_F_H
 #include <stdio.h>
 #include <stdarg.h>
-/**
- * struct token - The list of types of arguments passed to the function.
- * @token: The format.
- * @f: The Function (pointer to function).
-*/
-typedef struct token
-{
-	char *token;
-	/* va_list: It will hold information needed by other facilities. */
-	void (*f)(char *, va_list);
-} token_t;
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct format_token - The list of types of arguments passed to the function.
+ * @format_token: The format.
+ * @f_a: The Function (pointer to function).
+*/
+typedef struct format_token
+{
+	char *format_token;
+	/* va_list: It will hold information needed by other facilities. */
+	void (*f_a)(char *, va_list);
+} format_t;
 #endif /* V_F_H */
