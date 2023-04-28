fc41ba788e6e39c29cc431a99d22fc47b1e2de08
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-27 17:13:19 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /* The constructor function: */
-void constructor_main(void)__att__((construct));
+void _constructor(void) __attribute__ ((constructor));
 
 /**
- * constructor_main - Write a function that prints You're beat!
+ * _constructor - Write a function that prints You're beat!
  * and yet, you must allow,\nI bore my house upon my back!\n
  * before the main function is executed.
  */
-void constructor_main(void)
+void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
