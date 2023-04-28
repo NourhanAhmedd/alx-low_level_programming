e487358af00d15aa90da3e126f0456993bb1f2f6
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-27 17:29:30 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,5 @@
 #include "lists.h"
 
-/* Write a function that prints You're beat! and yet, */
-/* you must allow,\nI bore my house upon my back!\n */
-/* before the main function is executed. */
 void _constructor(void) __attribute__ ((constructor));
 
 /**
