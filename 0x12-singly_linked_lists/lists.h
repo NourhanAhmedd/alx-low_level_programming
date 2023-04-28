7a04bd3cd66c6ae1f42c44f434ebf23f239786e9
Nourhan Ahmed <nourhan222000@gmail.com>
2023-04-27 17:38:14 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,9 +22,9 @@ typedef struct list_s
 } list_t;
 
 /* The prototypes: */
-size_t print_list(const list_t *h);
 int _putchar(char character);
 int s_len(char *st);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
